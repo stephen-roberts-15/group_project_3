eb4a02ccf9cea38392158ad702ca8b4b7bbf8890
Stephen <fliggopolis@gmail.com>
2023-07-06 19:16:34 -0400

    added sql import file

---
+++ import_airbnb.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Airbnb (
+	index int unique primary key,
+	boroughs varchar(15) not null,
+	latitude float not null,
+	longitude float not null,
+	prop_type varchar(15) not null,
+	min_nights int,
+	Host_Listing_Cnt int,
+	Days_Available int,
+	Review_Cnt int,
+	Reviews float,
+	price float
+);
+
+select * from Airbnb;
+
+delete from Airbnb
+where not (Airbnb is not null);
