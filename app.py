b2fd60db97062c768c88bfbca1ec17165deed8d1
Jess Work <JessWork@Zacs-MBP.attlocal.net>
2023-07-10 20:59:26 -0400

    Adding files

--- app.py
+++ app.py
@@ -1,30 +1,43 @@
+# Middleman between data and hmtl 
+
 import numpy as np
 
 import pandas as pd
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template 
 import sqlite3
 
 
 
 conn = sqlite3.connect('Airbnb.sqlite')
-
+df = pd.read_sql('SELECT * FROM Airbnb', con=conn )
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
-@app.route("/")
-def welcome():
-    # List all available api routes.
-    return (
-    f"Welcome to the Airbnb API!"
-    f"Available Routes:<br/>"
-    )
+# @app.route("/")
+# def welcome():
+#     # List all available api routes.
+#     return (
+#     f"Welcome to the Airbnb API!"
+#     f"Available Routes:<br/>"
+#     )
 
-# @app.route("")
-    
+@app.route("/home") 
+def home():
+    conn = sqlite3.connect('Airbnb.sqlite')
+    df = pd.read_sql('SELECT * FROM Airbnb', con=conn )
+    return render_template("index.html")
+
+@app.route("/data") 
+def home():
+    conn = sqlite3.connect('Airbnb.sqlite')
+    df = pd.read_sql('SELECT * FROM Airbnb', con=conn )
+    return jsonify(df.to_json())
+
 
 
 if __name__ == "__main__": 
 
     app.run(debug = False)
+
