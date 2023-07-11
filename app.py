52cfc0c78779d19922cdfb005a9c2ccad145f5bd
Stephen <fliggopolis@gmail.com>
2023-07-10 20:02:30 -0400

    edited code

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
 import numpy as np
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+
 import pandas as pd
 from flask import Flask, jsonify
 import sqlite3
 
 
-#
 
 conn = sqlite3.connect('Airbnb.sqlite')
 
-engine = create_engine("sqlite:///Airbnb.sqlite")
 
-Base = automap_base()
-# reflect the tables
-Base.prepare(autoload_with=engine)
-Base.classes.keys()
-pd.read_sql("Select * from Airbnb", con = conn)
-# conn.close()
-################################################
-# Database Setup
-#################################################
-
-Airbnb = Base.classes.airbnb
-# Save reference to the table
-# Property = Base.classes.property
 #################################################
 # Flask Setup
 #################################################
@@ -35,5 +17,14 @@ app = Flask(__name__)
 def welcome():
     # List all available api routes.
     return (
- 
+    f"Welcome to the Airbnb API!"
+    f"Available Routes:<br/>"
     )
+
+# @app.route("")
+    
+
+
+if __name__ == "__main__": 
+
+    app.run(debug = False)
