d3b8f8e512001d6252ea9c4c57a9cd3a5534fca0
Stephen <fliggopolis@gmail.com>
2023-07-10 21:01:16 -0400

    modified app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 import pandas as pd
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import sqlite3
+import json
 
 
 
@@ -19,9 +20,15 @@ def welcome():
     return (
     f"Welcome to the Airbnb API!"
     f"Available Routes:<br/>"
+    
     )
 
-# @app.route("")
+@app.route("/data")
+def get_data():
+    conn = sqlite3.connect('Airbnb.sqlite')
+    df = pd.read_sql("Select * from Airbnb", con = conn)
+    return jsonify(df.to_json())
+    
     
 
 
