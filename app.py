5f52f2b0b2f62f4dcccfe41098faee703f2dae84
Nikitajain4 <127241094+Nikitajain4@users.noreply.github.com>
2023-07-14 14:37:09 -0400

    Update app.py with map items

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 import pandas as pd
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template ,url_for
 import sqlite3
 import json
 
 
+#Setting connection and reading json file
 
 conn = sqlite3.connect('Airbnb.sqlite')
-
+with open ("Resources/Airbnb_NYC.geojson")as NYC_Geo:
+    map_data = json.load(NYC_Geo)
 
 #################################################
 # Flask Setup
@@ -19,13 +21,20 @@ def welcome():
     # List all available api routes.
     return render_template('index.html')
 
+@app.route("/map_page")
+def mappage():
+   
+    return render_template('map.html')
+
 @app.route("/data")
 def get_data():
     conn = sqlite3.connect('Airbnb.sqlite')
     df = pd.read_sql("Select * from Airbnb", con = conn)
-    return jsonify(df.to_json())
+    return jsonify(df.to_json(orient="records"))
     
-# @app.route
+@app.route("/map")
+def get_data1():
+   return map_data
 
 
 if __name__ == "__main__": 
